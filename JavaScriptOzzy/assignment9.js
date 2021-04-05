c09ab99a7e278ca5e1823624a9ed903c15edd14b
tenadem <tenadem82@gmail.com>
2021-04-05 15:17:56 +0200

    assignment added

--- JavaScriptOzzy/assignment9.js
+++ JavaScriptOzzy/assignment9.js
@@ -39,5 +39,149 @@ for(var i = 2; i<=row;i++){
     console.log(print);
 }
 
+
+
+
+
+
+
+
+
+1
+1 2
+1 2 3
+1 2 3 4
+1 2 3 4 5
+1 2 3 4 5 6
+1 2 3 4 5 6 7
+1 2 3 4 5 6
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+
+var row =7;
+for(var i=1;i<=7;i++){
+    var print = "";
+    for(var j = 1; j <= i;j++ ){
+        print += j ;
+    }
+    console.log(print);
+}
+
+for (var i=1;i<=row-1;i++){
+    var print ="";
+    for(var j = 1;j <= row-i ; j++){
+        print += j ;
+    }
+    console.log(print);
+
+}
+
+
+
+
+
+
+
+
+
+1 2 3 4 5 6 7
+2 3 4 5 6 7
+ 3 4 5 6 7
+  4 5 6 7
+   5 6 7
+    6 7
+     7
+    6 7
+   5 6 7
+  4 5 6 7
+ 3 4 5 6 7
+2 3 4 5 6 7
+1 2 3 4 5 6 7
+
+
+var row = 7;
+for(var i=1;i<=row;i++){
+    var print = "";
+     for (var j=i ; j<=row;j++){
+         print += j+ " ";
+     }
+     for (var k=1;k<=i;k++){
+         print = " " + print;
+     }
+
+     console.log(print);
+}
+
+    for (var i=1;i<=row-1;i++){
+        var print = "";
+        for (var j = row-i;j<= row;j++){
+            print += j+ " "; 
+        }
+        for(var k = row-i; k >=1; k--){
+            print = " " + print;
+        }
+        console.log(print);
+    }
+
+
+
+
+
+
+1
+10
+101
+1010
+10101
+101010
+1010101
+
+
+var row = parseInt(prompt("Enter the row number"));
+
+for(var i=1; i<=row;i++){
+    var print = "";
+    for(var j = 1; j<=i;j++){
+        if(j%2!=0){
+            print += 1;
+        }else{
+            print += 0;
+        }
+    }
+    console.log(print);
+}
+
+
+
+
+
+
+
+
+1111111
+1111122
+1111333
+1114444
+1155555
+1666666
+7777777
+
 */
 
+
+var row = 10;
+for(var i = 1; i <= row ; i++){
+    var print = "";
+    for(var j= row;j>=1;j--){
+        if(j>i){
+            print+= 1;
+        }else{
+            print += i;
+        }
+    }
+    console.log(print);
+
+}

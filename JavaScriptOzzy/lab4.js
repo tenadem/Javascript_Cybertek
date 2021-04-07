36d46c70f045a03d32b366b63557be54ec1060ac
tenadem <tenadem82@gmail.com>
2021-04-07 05:04:40 +0200

    commit

---
+++ JavaScriptOzzy/lab4.js
@@ -0,0 +1,41 @@
+/*
+var times = 0;
+do{
+   alert("Enter 3 number");
+   var num1 = parseInt(prompt("Enter number 1"));
+   var num2 = parseInt(prompt("Enter number 2"));
+   var num3 = parseInt(prompt("Enter number 3"));
+
+   if(num1==num2 || num1==num3 || num2==num3){
+       alert("Numbers are equal");
+   }else{
+       if(num1>num2 && num1>num3){
+           alert(num1 + " is largest");
+       }else{
+           if(num2>num1 && num2>num3){
+            alert(num2 + " is largest"); 
+
+           }else{
+            alert(num3 + " is largest");
+           }
+       }
+   }
+   
+   var respond = parseInt(prompt("Do you want to continue:\n 1. Yes \n 2.No \n 3. Not sure"));
+   times++;
+}while(respond == 1)
+
+console.log("Thank you! you have tried " + times + " times");
+*/
+
+
+
+
+
+
+
+
+
+
+
+

ab6e4ad3b6c763d0ed527ac78702e006d67f2f1f
tenadem <tenadem82@gmail.com>
2021-04-12 00:05:03 +0200

    commit

--- JavaScriptOzzy/assignment10.js
+++ JavaScriptOzzy/assignment10.js
@@ -54,5 +54,40 @@ for(var i=0; i<arr.length;i++){
 console.log(arr);
 
 
+Question 5:
+In mathematics, the Fibonacci series are the numbers in the following integer sequence:
+0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ...
+By definition, the first two numbers in the Fibonacci sequence are 0 and 1, 
+and each subsequent number is the sum of the previous two.
+
+Write a JavaScript Program to generate above series till n number of times (For example you can try with 
+n = 10 and n = 15) and store it in One-dimensional array.
+For n = 10, Fibonacci series: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34
+For n = 15, Fibonacci series: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377
+
+Write a for loop to print all the elements of the array.
+
+*/
+var count = 12; 
+var str="";
+
+var fib = new Array(count); //Array to store the series
+fib[0] = 0; //First member of the series is always 0
+fib[1] = 1; //Second member of the series is always 1
+
+// Store the values in fib array
+for (var i = 2; i < count; i++) { 
+    fib[i] = fib[i - 1] + fib[i - 2]; //next number is always the sum of previous two numbers
+}
+
+// Print the values from fib array
+for (var i = 0; i < fib.length; i++) {
+    if(i==fib.length-1){
+       str=str+fib[i];
+    }else{
+        str=str+fib[i] +",";
+    }
+}
+console.log(str);
 
 
